ad74a92431e498b07b6b6eaa3acea35a9d8998f9
KayLau <125188052+Rap1zer@users.noreply.github.com>
2025-10-04 23:58:39 +0100

    Rearrange files and folders

--- Assets/Scripts/Items/IItemBehaviour.cs
+++
@@ -1,8 +0,0 @@
-using System;
-using UnityEngine;
-
-public interface IItemBehaviour
-{
-    public virtual void OnEquip(PlayerController player) { }
-    public virtual void OnUnequip(PlayerController player) { }
-}
--- Assets/Scripts/Items/IOnMiss.cs
+++
@@ -1,4 +0,0 @@
-public interface IOnMiss
-{
-    public void OnMiss() { }
-}
