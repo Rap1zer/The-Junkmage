142f023d0c1d9b5569beb96dc954035ef40f246f
KayLau <125188052+Rap1zer@users.noreply.github.com>
2025-10-04 17:46:39 +0100

    Implement fizzy drink

---
+++ Assets/Scripts/Items/FizzyDrink.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FizzyDrink : ItemBase
+{
+    private PlayerStats stats;
+
+    private StatModifier modifier;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        stats = GameObject.Find("Player").GetComponent<PlayerStats>();
+        modifier = new StatModifier(StatType.AttackCooldown, 0.25f, ModifierType.Flat);
+    }
+
+    protected override void OnEquip()
+    {
+        base.OnEquip();
+        stats.ApplyModifier(modifier);
+    }
+
+    protected override void OnUnequip()
+    {
+        base.OnUnequip();
+        stats.RemoveModifier(modifier);
+    }
+}
